72498e33ad35214cc1cf629c805a047b682dcf7c
victorlfdev <victorlf2002@gmail.com>
2026-09-23 14:33:17 -0300

    chore(server): remover código antigo substituído

--- server/src/db/seed.js
+++
@@ -1,8 +0,0 @@
-const { getDb } = require('./database');
-
-async function ensureSeeded() {
-  await getDb();
-  console.log('Database initialized and seeded (handled by database.js init)');
-}
-
-ensureSeeded();
